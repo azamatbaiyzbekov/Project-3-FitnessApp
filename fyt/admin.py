93b52e4006ac84f8b82b9821abed00b200bb5bd3
pouyarezvani1 <pouyarezvani10@gmail.com>
2019-08-08 11:21:03 -0700

    Deletes Exercise Model

--- fyt/admin.py
+++ fyt/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import Workout, Exercise
+from .models import Workout
 
 # Register your models here.
 
 admin.site.register(Workout)
-admin.site.register(Exercise)
--- fyt/views.py
+++ fyt/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, JsonResponse
+from .models import Workout
+from .forms import WorkoutForm
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+
+def home(request):
+    return HttpResponse('This is home')
+
+def json_res(request):
+    return render({"status": "Ok"})
+
+def workout_list(request):
+    workouts = Workout.objects.all()
+    return render(request, 'workout_list.html', {"workouts": workouts})
+
+
+def workout_detail(request, pk):
+    workout = Workout.objects.get(id=pk)
+    return render(request, 'workout_detail.html', {"workout": workout})
+
+# SECTION Create exercise controller
