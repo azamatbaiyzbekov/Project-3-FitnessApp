fc83a7d47fe9482f88e38b59580a5745e1c77bab
Azamat Baiyzbekov <azamatbaiyzbekov@Azamats-MacBook-Pro.local>
2019-08-08 11:13:20 -0700

    adds register function

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render,
+from django.contrib import auth
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 def register(request):
-    pass
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
 
-def login(request):
-    pass
+        if password == password2:
+            if User.objects.filter(username=username).exists():
+                return render(request, 'register.html', {'error': 'Username is take'})
+            else:
+                if User.objects.filter(email=email).exists():
+                    return render(request, 'register.html', {'error': 'Email already exists'})
+                else:
+                    user = User.objects.create_user (
+                        username=username, password=password, email=email, first_name=first_name, last_name=last_name
+                    )
+                    user.save()
+                    return redirect('login')
+        else:
+            return render(request, 'register.html', {'error': 'Passwords do not match'})
+    
+    else:
+        return render(request, 'register.html')
 
-def logout(request):
-    pass
+
+
+
