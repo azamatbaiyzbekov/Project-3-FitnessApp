b5b969be806e96e745572c6723a48be1be383c35
pouyarezvani1 <pouyarezvani10@gmail.com>
2019-08-08 12:27:43 -0700

    Adds full CRUD to views

--- fyt/urls.py
+++ fyt/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
 from . import views
 
+# SECTION URL Patterns and routes
+
 urlpatterns = [
-    # path('', views.home, name="home")
+    path('', views.workout_list, name='workout_list'),
+    path('workouts/<int:pk>', views.workout_detail, name='workout_detail'),
+    path('workouts/<int:pk>/edit', views.workout_edit, name='workout_edit'),
+    path('workouts/<int:pk>/delete', views.workout_delete, name='workout_delete'),
+    path('workouts/new', views.workout_create, name='workout_create'),
+    path('profile/', views.profile, name='profile'),
+    path('about/', views.about, name='about'),
 ]
+
--- fyt/views.py
+++ fyt/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse
 from .models import Workout
 from .forms import WorkoutForm
 from django.contrib.auth.decorators import login_required
@@ -10,16 +10,50 @@ from django.contrib.auth.decorators import login_required
 def home(request):
     return HttpResponse('This is home')
 
-def json_res(request):
-    return render({"status": "Ok"})
-
 def workout_list(request):
     workouts = Workout.objects.all()
     return render(request, 'workout_list.html', {"workouts": workouts})
-
 
 def workout_detail(request, pk):
     workout = Workout.objects.get(id=pk)
     return render(request, 'workout_detail.html', {"workout": workout})
 
-# SECTION Create exercise controller
+# NOTE Create workout controller
+
+def workout_create(request):
+    if request.method == 'POST':
+        form = WorkoutForm(request.POST)
+        if form.is_valid():
+            workout = form.save(commit=False)
+            workout.user = request.user
+            workout.save()
+            return redirect('workout_detail', pk=workout.pk)
+    else: 
+        form = WorkoutForm()
+    return render(request, 'workout_form.html', {'form': form, 'header': 'New Workout'})
+
+# NOTE Edit Workout Controller
+
+def workout_edit(request, pk):
+    workout = Workout.objects.get(id=pk)
+    if request.method == 'POST':
+        form = WorkoutForm(request.POST, instance=workout)
+        if form.is_valid():
+            workout = form.save()
+            return redirect('workout_detail', pk=workout.pk)
+    else:
+        form = WorkoutForm(instance=workout)
+    return render(request, 'workout_form.html', {'form': form, 'header':f'Edit {workout.workout_name}'})
+
+# NOTE Delete Workout Controller
+
+def workout_delete(request, pk):
+    Workout.objects.get(id=pk).delete()
+    return redirect('workout_list')
+  
+# NOTE User Profile Controller
+
+def profile(request):
+    user = request.user
+    workouts = Workout.objects.filter(user=user)
+    return render(request, 'profile.html', {'workouts': workouts})
