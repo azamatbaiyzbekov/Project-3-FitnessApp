0a8b9a7fdcae3397b73b16cdaef76f21c882b72b
Azamat Baiyzbekov <azamatbaiyzbekov@Azamats-MacBook-Pro.local>
2019-08-08 11:31:24 -0700

    adds login function

--- accounts/views.py
+++ accounts/views.py
@@ -33,4 +33,19 @@ def register(request):
 
 
 
+def login(request):
 
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('#')
+
+        else:
+            return render(request, 'login.html' {'error': 'Invalid Credentials'})
+    else:
+        return render(request, 'login.html')
