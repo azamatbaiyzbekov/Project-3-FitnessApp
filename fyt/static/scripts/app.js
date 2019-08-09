49a7b08ffda18c5ebcc80dd719b97662f20175fe
pouyarezvani1 <pouyarezvani10@gmail.com>
2019-08-09 15:56:00 -0700

    Adds likes and workout detail

--- fyt/static/scripts/app.js
+++ fyt/static/scripts/app.js
@@ -1,1 +1,14 @@
 console.log('really really fyt')
+
+$('.btn').on('click', function(event){
+  event.preventDefault();
+  const element = $(this);
+  $.ajax({
+    url: '/like_workout/',
+    method: 'GET',
+    data: {workout_id: element.attr('data-id')},
+    success: function(response){
+      element.html('<i class="far fa-heart"></i> ' + response);
+    }
+  })
+})
